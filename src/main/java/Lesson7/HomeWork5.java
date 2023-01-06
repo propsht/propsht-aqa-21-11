c57132a8427b1d7054a175dd03f4f1a1a64032dd
propsht <igor.propsht@gmail.com>
2023-01-06 20:34:06 +0100

    home work 5 final minor diagonal in matrix add print

--- src/main/java/Lesson7/HomeWork5.java
+++ src/main/java/Lesson7/HomeWork5.java
@@ -2,7 +2,7 @@ package Lesson7;
 
 public class HomeWork5 {
  public static void main(String[] args) {
-
+        int i = 0, j = 0;
         int[][] array = {
                 {1,  2,  3,  4,  5},
                 {6,  7,  8,  9,  10},
@@ -10,25 +10,20 @@ public class HomeWork5 {
                 {16, 17, 18, 19, 20},
                 {21, 22, 23, 24, 25}
         };
-        int i = 0, j = 0;
 
+        System.out.print("Major Diagonal in Matrix= ");
         for (i = 0; i < array.length; i++) {
-            System.out.print(array[i] [i] + " ");
+            System.out.print( + array[i] [i] + " ");
 
         }
 
         System.out.println();
+        System.out.print("Minor Diagonal in Matrix= ");
 
         for (i = 0, j = array.length-1; i < array.length && j >= 0; i++, j--) {
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
-
-
-
-
-
-
 
     }
 
