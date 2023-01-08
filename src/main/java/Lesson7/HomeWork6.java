c08f30b57f98c71479e3c0cfff43812bb1e777f8
Igor <igor.propsht@gmail.com>
2023-01-08 20:50:56 +0100

    Hw6 matrix summ in row fix name

--- src/main/java/Lesson7/HomeWork6.java
+++ src/main/java/Lesson7/HomeWork6.java
@@ -1,29 +1,28 @@
 package Lesson7;
 public class HomeWork6 {
-    static int[][] array;
+    static int[][] sourse;
     int i = 0;
     int j = 0;
 
     //инициализация и вывод массива
     public static void main(String[] args) {
-        int[][] array = {
+        int[][] sourse = {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 5}
         };
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+        for (int i = 0; i < sourse.length; i++) {
+            for (int j = 0; j < sourse[i].length; j++) {
+                System.out.print(sourse[i][j] + " ");
             }
             System.out.println();
         }
-        sumsInRows(array);
+        sumsInRows(sourse);
     }
 
 //вычисление сумм
         public static int[] sumsInRows (int[][] sourse) {
-
         int sumsInRows;
 
         for (int i = 0; i < sourse.length; i++) {
@@ -33,7 +32,6 @@ public class HomeWork6 {
             }
             System.out.println("Summ in  row " + i + " is " + sumsInRows);
         }
-
 
         return new int[0];
         }
