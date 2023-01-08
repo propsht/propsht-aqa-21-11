cd152b6d655fc4f9cadba049e0ecf0484d0c28e7
Igor <igor.propsht@gmail.com>
2023-01-08 20:48:29 +0100

    Hw6 matrix summ in row

--- src/main/java/Lesson7/HomeWork6.java
+++ src/main/java/Lesson7/HomeWork6.java
@@ -1,40 +1,49 @@
 package Lesson7;
-
 public class HomeWork6 {
-    public static void main(String[] args) {
+    static int[][] array;
+    int i = 0;
+    int j = 0;
 
-        // i=строка j=столбец
+    //инициализация и вывод массива
+    public static void main(String[] args) {
         int[][] array = {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 5}
         };
 
-
-        for (int i = 0; i <= array.length - 1; i++) {
-            for (int j = 0; j <= array[i].length - 1; j++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
         }
+        sumsInRows(array);
+    }
+
+//вычисление сумм
+        public static int[] sumsInRows (int[][] sourse) {
+
+        int sumsInRows;
+
+        for (int i = 0; i < sourse.length; i++) {
+            sumsInRows = 0;
+            for (int j = 0; j < sourse[i].length; j++) {
+                sumsInRows = sumsInRows + sourse[i][j];
+            }
+            System.out.println("Summ in  row " + i + " is " + sumsInRows);
+        }
 
 
-//        int[] sumsInRows(array)
+        return new int[0];
+        }
 
 
-
-
-//    public static int[] sumsInRows (int[][] sourse) {
-//
-//            for (int i = 0; i <= array.length - 1; i++) {
-//            int sumsInRows = 0;
-//            for (int j = 0; j <= array[i].length - 1; j++)
-//                sumsInRows = sumsInRows + array[i][j];
-//            System.out.print(sumsInRows + " ");
-//
-//        }
-//    }
-
-   }
 }
+
+
+
+
+
+
 
--- src/main/java/lesson_8/Task2.java
+++ src/main/java/lesson_8/Task2.java
@@ -1,23 +1,23 @@
 package org.brit.lesson8;
 
 public class Task2 {
-    public static void main(String[] args) {
-        int[] array = ArrayUtils.generateArray(10, -15, 15);
-        ArrayUtils.printArray(array);
-        int maxIndex = minElementIndex(array);
-        System.out.println(maxIndex);
-    }
-
-    public static int minElementIndex(int[] array) {
-        int index = 0;
-        int max = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] >= max) {
-                max = array[i];
-                index = i;
-            }
-        }
-        return index;
-    }
+//    public static void main(String[] args) {
+//        int[] array = ArrayUtils.generateArray(10, -15, 15);
+//        ArrayUtils.printArray(array);
+//        int maxIndex = minElementIndex(array);
+//        System.out.println(maxIndex);
+//    }
+//
+//    public static int minElementIndex(int[] array) {
+//        int index = 0;
+//        int max = array[0];
+//        for (int i = 1; i < array.length; i++) {
+//            if (array[i] >= max) {
+//                max = array[i];
+//                index = i;
+//            }
+//        }
+//        return index;
+//    }
 
 }
