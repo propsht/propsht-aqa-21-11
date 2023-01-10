e787b010b8079a5f5043d78a34bcae25a819d880
propsht <igor.propsht@gmail.com>
2023-01-10 23:30:50 +0100

    home works 6 fix

--- src/main/java/Lesson7/HomeWork6.java
+++ src/main/java/Lesson7/HomeWork6.java
@@ -3,8 +3,34 @@ public class HomeWork6 {
     static int[][] sourse;
     int i = 0;
     int j = 0;
+    static int[] sumsInRows;
 
-    //инициализация и вывод массива
+
+
+    //вычисление сумм
+    public static int[] sumsInRows (int[][] sourse) {
+
+        int[] sumsInRows = new int[sourse.length];
+
+
+        for (int i = 0; i < sourse.length; i++) {
+            sumsInRows[i] = 0;
+            for (int j = 0; j < sourse[i].length; j++) {
+                sumsInRows[i] = sumsInRows[i] + sourse[i][j];
+            }
+           System.out.print(sumsInRows[i] + ", " );
+
+
+        }
+
+        //return new int[]{sumsInRows[i]};
+        return sumsInRows;
+
+
+    }
+
+
+
     public static void main(String[] args) {
         int[][] sourse = {
                 {1, 2, 3},
@@ -17,27 +43,14 @@ public class HomeWork6 {
                 System.out.print(sourse[i][j] + " ");
             }
             System.out.println();
+
         }
+        System.out.print("Sums in  rows  are = ");
         sumsInRows(sourse);
+
     }
-
-//вычисление сумм
-        public static int[] sumsInRows (int[][] sourse) {
-        int sumsInRows;
-
-        for (int i = 0; i < sourse.length; i++) {
-            sumsInRows = 0;
-            for (int j = 0; j < sourse[i].length; j++) {
-                sumsInRows = sumsInRows + sourse[i][j];
-            }
-            System.out.println("Summ in  row " + i + " is " + sumsInRows);
-        }
-
-        return new int[0];
-        }
-
-
 }
+
 
 
 
